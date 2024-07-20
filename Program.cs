4532f3b524dc7c167ada4d70315fc2e0e6ed5ac7
Naser <Naser.Masri@epact.be>
2024-07-20 22:35:46 +0200

    Add excute data

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace ExcuteRawSql;
+class Program
+{
+    public static void Main()
+    {
+        var confi = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+        //Console.WriteLine(confi.GetSection("constr").Value);
+        var connectionStr = confi.GetSection("constr").Value;
+        SqlConnection conn = new SqlConnection(connectionStr);
+
+        var sql = "SELECT * FROM WALLETS";
+
+        SqlCommand cmd = new SqlCommand(sql, conn);
+
+        cmd.CommandType = CommandType.Text; 
+
+        conn.Open();
+
+        SqlDataReader reader = cmd.ExecuteReader();
+
+        Wallet wallate;
+
+        while (reader.Read())
+        {
+            wallate = new Wallet
+            {
+                Id = reader.GetInt32("Id"),
+                Holder = reader.GetString("Holder"),
+                Balance = reader.GetDecimal("Balance"),
+            };
+            Console.WriteLine(wallate);
+        }
+        conn.Close();
+        Console.ReadKey();
+    }
+}
