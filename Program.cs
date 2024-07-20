74192a90c7816a4c2461064ba08418243760857e
Naser <Naser.Masri@epact.be>
2024-07-20 22:09:27 +0200

    get connection string.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+
+namespace Currency;
+
+class Program
+{
+    public static void Main()
+    {
+        var confi = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+        Console.WriteLine(confi.GetSection("constr").Value);
+
+        Console.ReadKey();
+    }
+}
